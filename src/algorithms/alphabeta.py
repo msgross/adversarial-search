6092bb500ecfd113cf9d0b25860e2be1575f620e
mgross <mark@grossremarks.com>
2021-10-16 12:09:49 -0700

    Completed initial impl of alphabeta search
    Need to test it, but adds a min and max value method
    Along the lines of psuedo-impl in Stuart and Russel

--- src/algorithms/alphabeta.py
+++ src/algorithms/alphabeta.py
@@ -3,5 +3,37 @@ from search import Search
 
 
 class AlphaBeta(Search):
+
+    def _max_value(self, game, state, player, alpha=-math.inf, beta=math.inf):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        best_value = -math.inf
+        best_move = None
+        for action in game.actions(state):
+            value, move = self._min_value(game, game.result(state, action), alpha, beta)
+            if value > best_value:
+                best_value = value
+                best_move = move
+                alpha = max(alpha, best_value)
+            if best_value >= beta:
+                return best_value, best_move
+        return best_value, best_move
+
+    def _min_value(self, game, state, player, alpha=-math.inf, beta=math.inf):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        best_value = math.inf
+        best_move = None
+        for action in game.actions(state):
+            value, move = self._max_value(game, game.result(state, action), alpha, beta)
+            if value < best_value:
+                best_value = value
+                best_move = move
+            if best_value <= alpha:
+                return best_value, best_move
+        return best_value, best_move
+
     def search(self, game, state, depth=math.inf, time_remaining=math.inf):
-        pass
+        current_player = game.player_turn(state)
+        value, move = self._max_value(game, state,current_player)
+        return move
