83bfd3ac74629de484f8e518f70b85fd6cd7ca6a
mgross <mark@grossremarks.com>
2021-10-16 14:08:03 -0700

    Added debug display method
    For outputting string forms of the current game state

--- src/algorithms/random_choice.py
+++ src/algorithms/random_choice.py
@@ -5,6 +5,5 @@ from search import Search
 
 class RandomChoice(Search):
     def search(self, game, state, depth_remaining=math.inf, time_remaining=math.inf):
-        possible_moves = game.actions(state)
-        return random.choice(possible_moves)
+        return math.inf, random.choice(game.actions(state))
 
--- src/game.py
+++ src/game.py
@@ -5,18 +5,26 @@ class _GameMeta(type):
         return cls.__subclasscheck__(type(instance))
 
     def __subclasscheck__(cls, subclass):
-        return (hasattr(subclass, 'search')) and callable(subclass.search)
+        return ((hasattr(subclass, 'is_terminal')) and callable(subclass.is_terminal)) and \
+               ((hasattr(subclass, 'result')) and callable(subclass.result)) and \
+               ((hasattr(subclass, 'actions')) and callable(subclass.actions))
 
 
 class Game(metaclass=_GameMeta):
-    def is_terminal(self, state, has_depth, has_time):
+    def is_terminal(self, state, has_depth, has_time) -> bool:
         pass
 
     def result(self, state, action):
         pass
 
-    def actions(self, state):
+    def actions(self, state) -> []:
         pass
 
-    def eval(self, state):
+    def eval(self, state) -> float:
         pass
+
+    def debug_display(self, state) -> str:
+        pass
+
+
+
