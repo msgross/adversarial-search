dd3bc2be732f29de45faeed944378206e133afae
mgross <mark@grossremarks.com>
2021-10-16 12:23:49 -0700

    Added random choice
    Just pick a random move from those that are there

--- src/algorithms/random_choice.py
+++ src/algorithms/random_choice.py
@@ -1,8 +1,10 @@
 import math
+import random
 from search import Search
 
 
 class RandomChoice(Search):
     def search(self, game, state, depth=math.inf, time_remaining=math.inf):
-        pass
+        possible_moves = game.actions(state)
+        return random.choice(possible_moves)
 
