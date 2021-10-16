0f20af77fa9ef7b14a73fef401c12ac8e46c0eae
mgross <mark@grossremarks.com>
2021-10-16 13:32:44 -0700

    Moved game current_player to be in state
    Have the state maintain the current player as part of its state

--- src/algorithms/alphabeta.py
+++ src/algorithms/alphabeta.py
@@ -5,13 +5,15 @@ from search import Search
 class AlphaBeta(Search):
 
     def _max_value(self, game, state, player, depth_remaining, time_remaining, alpha=-math.inf, beta=math.inf):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
+        if game.is_terminal(state, depth_remaining > 0, time_remaining > 0):
+            return game.eval(state, player), None
         best_value = -math.inf
         best_move = None
         for action in game.actions(state):
-            value, move = self._min_value(game, game.result(state, action),
-                                          depth_remaining, time_remaining,
+            state_after_action = game.result(state, action)
+            value, move = self._min_value(game, state_after_action,
+                                          state_after_action.current_player(),
+                                          depth_remaining-1, time_remaining,
                                           alpha, beta)
             if value > best_value:
                 best_value = value
@@ -22,13 +24,16 @@ class AlphaBeta(Search):
         return best_value, best_move
 
     def _min_value(self, game, state, player, depth_remaining, time_remaining,  alpha=-math.inf, beta=math.inf):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
+
+        if game.is_terminal(state, depth_remaining > 0, time_remaining > 0):
+            return game.eval(state, player), None
         best_value = math.inf
         best_move = None
         for action in game.actions(state):
-            value, move = self._max_value(game, game.result(state, action),
-                                          depth_remaining, time_remaining,
+            state_after_action = game.result(state, action)
+            value, move = self._max_value(game, state_after_action,
+                                          state_after_action.current_player(),
+                                          depth_remaining-1, time_remaining,
                                           alpha, beta)
             if value < best_value:
                 best_value = value
@@ -38,6 +43,6 @@ class AlphaBeta(Search):
         return best_value, best_move
 
     def search(self, game, state, depth_remaining=math.inf, time_remaining=math.inf):
-        current_player = game.player_turn(state)
+        current_player = state.current_player()
         value, move = self._max_value(game, state, current_player, depth_remaining, time_remaining)
         return move
--- src/algorithms/minimax.py
+++ src/algorithms/minimax.py
@@ -4,32 +4,34 @@ from search import Search
 
 class Minimax(Search):
     def _max_value(self, game, state, player, depth_remaining, time_remaining):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
+        if game.is_terminal(state, depth_remaining > 0, time_remaining > 0):
+            return game.eval(state, player), None
         best_value = -math.inf
         best_move = None
         for action in game.actions(state):
-            value, move = self._min_value(game, game.result(state, action), player,
-                                          depth_remaining, time_remaining)
+            state_after_action = game.result(state, action)
+            value, move = self._min_value(game, state_after_action, state_after_action.current_player(),
+                                          depth_remaining-1, time_remaining)
             if value > best_value:
                 best_value = value
                 best_move = move
         return best_value, best_move
 
     def _min_value(self, game, state, player, depth_remaining, time_remaining):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
+        if game.is_terminal(state, depth_remaining > 0, time_remaining > 0):
+            return game.eval(state, player), None
         best_value = math.inf
         best_move = None
         for action in game.actions(state):
-            value, move = self._max_value(game, game.result(state, action), player,
-                                          depth_remaining, time_remaining)
+            state_after_action = game.result(state, action)
+            value, move = self._max_value(game, state_after_action, state_after_action.current_player(),
+                                          depth_remaining-1, time_remaining)
             if value < best_value:
                 best_value = value
                 best_move = move
         return best_value, best_move
 
     def search(self, game, state, depth_remaining=math.inf, time_remaining=math.inf):
-        current_player = game.player_turn(state)
+        current_player = state.current_player()
         value, move = self._max_value(game, state, current_player, depth_remaining, time_remaining)
         return move
