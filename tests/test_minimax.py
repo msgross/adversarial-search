cfbb55c1ceac09d27f3ec9dedc4739c61252fe60
mgross <mark@grossremarks.com>
2021-10-17 10:59:57 -0700

    Initialize unittest for minimax

---
+++ tests/test_minimax.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestMinimax(TestCase):
+    def test__max_value(self):
+        self.fail()
+
+    def test__min_value(self):
+        self.fail()
+
+    def test_search(self):
+        self.fail()
