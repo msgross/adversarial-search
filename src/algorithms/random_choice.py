6a5feac16de9414271307b07631369c92337c450
mgross <mark@grossremarks.com>
2021-10-16 18:02:20 -0700

    Added some catch statements for no avail moves
    Just return None if the moves are gone

--- src/algorithms/random_choice.py
+++ src/algorithms/random_choice.py
@@ -1,9 +1,15 @@
 import math
 import random
-from search import Search
+
+from src.algorithms.search import Search
 
 
 class RandomChoice(Search):
     def search(self, game, state, depth_remaining=math.inf, time_remaining=math.inf):
+        if game.is_terminal(state, True, True):
+            return math.inf, None
+        available_actions = game.actions(state)
+        if len(available_actions) == 0:
+            return math.inf, None
         return math.inf, random.choice(game.actions(state))
 
