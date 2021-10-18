275e20f572b9315e4983689e7f7e86686ded1d9e
mgross <mark@grossremarks.com>
2021-10-17 20:54:37 -0700

    Created a test for minimax search alg
    Build a graph and then make sure we end up in the
    right place once we finish evaluating

--- tests/test_minimax.py
+++ tests/test_minimax.py
@@ -1,12 +1,167 @@
 from unittest import TestCase
+from mock import create_autospec
+from src.game import Game
+from src.algorithms.minimax import Minimax
+from math import inf
+
+def terminal_side_effect(state, depth_remaining, time_remaining):
+    """ Side effect returns true if we reach a terminal state
+
+    :param state: the state of the game to evaluate
+    :param depth_remaining: the remaining depth before we stop evaluating
+    :param time_remaining: the time remaining before we stop evaluating
+    :return: true if we are terminating
+    """
+    end_state_nodes = []
+    for alpha in list(map(chr, range(101, 110))):  # iterate from e-m
+        end_state_nodes.append(str(alpha))
+    if state in end_state_nodes:
+        return True
+    return False
+
+
+def eval_side_effect(state):
+    match state:
+        case "a":
+            return inf  # this is our root note, it shouldn't get a score
+        case "b":
+            return 3
+        case "c":
+            return 2
+        case "d":
+            return 2
+        case "e":
+            return 3
+        case "f":
+            return 12
+        case "g":
+            return 8
+        case "h":
+            return 2
+        case "i":
+            return 4
+        case "j":
+            return 6
+        case "k":
+            return 14
+        case "l":
+            return 5
+        case "m":
+            return 2
+        case _:
+            return -inf  # this shouldn't happen in our simple case
+
+
+def actions_side_effect(state):
+    match state:
+        case "a":
+            return ["a1", "a2", "a3"]
+        case "b":
+            return ["b1", "b2", "b3"]
+        case "c":
+            return ["c1", "c2", "c3"]
+        case "d":
+            return ["d1", "d2", "d3"]
+        case "e":
+            return []
+        case "f":
+            return []
+        case "g":
+            return []
+        case "h":
+            return []
+        case "i":
+            return []
+        case "j":
+            return []
+        case "k":
+            return []
+        case "l":
+            return []
+        case "m":
+            return []
+        case _:
+            return None  # This shouldn't happen
+
+
+def result_side_effect(state, action):
+    match action:
+        # We would normally evaluate this with the
+        # state to determine validity, etc, but this is a pre-established graph
+        case "a1":
+            return "b"
+        case "a2":
+            return "c"
+        case "a3":
+            return "d"
+        case "b1":
+            return "e"
+        case "b2":
+            return "f"
+        case "b3":
+            return "g"
+        case "c1":
+            return "h"
+        case "c2":
+            return "i"
+        case "c3":
+            return "j"
+        case "d1":
+            return "k"
+        case "d2":
+            return "l"
+        case "d3":
+            return "m"
+        case _:
+            return None  # default case, shouldn't happen
 
 
 class TestMinimax(TestCase):
-    def test__max_value(self):
-        self.fail()
+    """ Define a graph we know the answer to, make sure we hit the right answer
 
-    def test__min_value(self):
-        self.fail()
+                                          a
+                                 a1   /---|----\    a3
+                              /-------    |     -------\
+                          ----         a2 |             ----
+                          |               |                |
+                          |               |                |
+                        3 b             2 c              2 d
+                        /-|-\            /|\              /|\
+                    b1 /  |  \ b3       / | \ c3     d1  / | \ d3
+                      / b2|   \      c1/  |  \          /  |  \
+                     /    |    \      / c2|   \        / d2|   \
+                    /     |     \    /    |    \      /    |    \
+                   -             -  -           -    -     |     -
+                 3 e    12f    8 g  2 h   4i    6j   14k   5l    2m
+        Note that node k, with a score of 14 doesn't get selected. This is because
+        we assume that the opponent is trying to pick the optimal means to minimize
+        the score, so we take the conservative route of picking a1 since it has the
+        'least worst' outcome
 
-    def test_search(self):
-        self.fail()
+        If you were to adjust node m to have a terminal value of 3, it would still
+        return the a1, even if there's a greater potential node if we took a3, since
+        we assume the opponent will select the optimal move on their end, they're
+        equivalent
+
+    """
+    def setUp(self):
+        self.mock_game = create_autospec(Game)
+        self.mock_game.is_terminal.side_effect = terminal_side_effect
+        self.mock_game.eval.side_effect = eval_side_effect
+        self.mock_game.actions.side_effect = actions_side_effect
+        self.mock_game.result.side_effect = result_side_effect
+        self.minimax_search = Minimax()
+
+    def test_search_from_root(self):
+        value, move = self.minimax_search.search(self.mock_game, "a")
+        self.assertEqual("a1", move, "This graph should determine a1 to be the optimal move")
+        self.assertEqual(3, value, "Valuation of this move is 3")
+
+    def test_search_near_terminal(self):
+        value, move = self.minimax_search.search(self.mock_game, "b")
+        self.assertEqual("b2", move, "Starting at node b should just return the move to the maximum leaf node")
+        self.assertEqual(12, value, "Valuation of this move is 12")
+
+    def test_search_at_terminal(self):
+        value, move = self.minimax_search.search(self.mock_game, "e")
+        self.assertIsNone(move)
