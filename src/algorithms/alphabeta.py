00da30e8574c605639194b60ca5a4b0f15f24882
mgross <mark@grossremarks.com>
2021-10-20 20:44:37 -0700

    Added some rough comments on the alphabeta search
    Need to go back to make sure I explain the alpha
    and beta values are PROPERLY explained

--- src/algorithms/alphabeta.py
+++ src/algorithms/alphabeta.py
@@ -3,8 +3,24 @@ from src.algorithms.search import Search
 
 
 class AlphaBeta(Search):
-
+    """ Implements a basic Alpha-Beta pruning search
+        Meant to cut down on searches needed by keeping track
+        of the min and max encountered in past branches
+    """
     def _max_value(self, game, state, depth_remaining, time_remaining, alpha=-inf, beta=inf):
+        """ Method returns a maximizing value in the state tree that surpasses the current
+            beta value if there is one
+
+        :param game: The game, dictates game rules
+        :param state: The current game state
+        :param depth_remaining: The remaining depth to search
+        :param time_remaining:  The remaining time
+        :param alpha: the value of the most maximizing choice we've found--min searches should
+                        at least reach this level
+        :param beta: the value of the most minimizing choice we've found--max searches should at most reach
+                        this level
+        :return: value, move tuple that is maximizing
+        """
         if game.is_terminal(state, depth_remaining > 0, time_remaining > 0):
             return game.eval(state), None
         best_value = -inf
@@ -23,7 +39,19 @@ class AlphaBeta(Search):
         return best_value, best_move
 
     def _min_value(self, game, state, depth_remaining, time_remaining,  alpha=-inf, beta=inf):
+        """ Method returns a minimizing value in the state tree that surpasses the current
+            alpha value if there is one
 
+        :param game: The game, dictates game rules
+        :param state: The current game state
+        :param depth_remaining: The remaining depth to search
+        :param time_remaining:  The remaining time
+        :param alpha: the value of the most maximizing choice we've found--min searches should
+                        at least reach this level
+        :param beta: the value of the most minimizing choice we've found--max searches should at most reach
+                        this level
+        :return: value, move tuple that is maximizing
+        """
         if game.is_terminal(state, depth_remaining > 0, time_remaining > 0):
             return game.eval(state), None
         best_value = inf
@@ -42,4 +70,5 @@ class AlphaBeta(Search):
         return best_value, best_move
 
     def search(self, game, state, depth_remaining=inf, time_remaining=inf):
+        """ Overrides Search.search """
         return self._max_value(game, state, depth_remaining, time_remaining)
