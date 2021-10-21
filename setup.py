fadd0c542db59f671ebd481413650e28d5d2ce5c
mgross <mark@grossremarks.com>
2021-10-20 21:11:08 -0700

    Adding some setup and requirement files
    Just an initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Adversarial_Search',
+    url='https://github.com/msgross/adversarial-searching',
+    author='Mark Gross',
+    author_email='mark@grossremarks.com',
+    packages=['algorithms'],
+    install_requires=[],
+    version='0.1',
+    license='MIT',
+    description='Implements a couple adversarial searches: random, minimax, alphabeta',
+    long_description=open('README').read()
+)
