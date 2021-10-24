da41934e0345f72a5f2568f9fbea4034ca02af3b
mgross <mark@grossremarks.com>
2021-10-23 23:16:24 -0700

    Added comments to node class

--- node.py
+++ node.py
@@ -1,20 +1,44 @@
+""" Node class, can be assembled into a tree
+
+"""
 
 
 class Node:
+    """ A node in a tree
 
+    """
     def __init__(self, value):
+        """ Constructor
+
+        :param value: value that will be wrapped in a node as a child of this node
+        """
         self.value = value
         self.children = []
 
     def add_child(self, node_value):
+        """ Add a new node as a child of this node
+
+        :param node_value: the node value to be wrapped in a node and added as
+                            a child of this node
+        :return: the newly created node
+        """
         new_node = Node(node_value)
         self.children.append(new_node)
         return new_node
 
     def is_leaf(self) -> bool:
+        """ Method returns true if this node is a leaf
+
+        :return: True if this node has no children, False otherwise
+        """
         return len(self.children) == 0
 
     def find(self, val):
+        """ Method searches for a given value within this node and its children
+
+        :param val: the value being searched for
+        :return: the node containing the value
+        """
         if self.value is val:
             return self
         for child in self.children:
@@ -24,12 +48,24 @@ class Node:
         return None
 
     def get_children(self):
+        """ Getter for children list
+
+        :return: the list of children for this node
+        """
         return self.children
 
     def get_value(self):
+        """ Getter for value
+
+        :return: the value of this node
+        """
         return self.value
 
     def debug_print(self):
+        """ A debugging printer, prints a formatted tree
+
+        :return: void
+        """
         print(str(self.value) + ":")
         for child in self.children:
             print(str(child.get_value()) + "\t")
@@ -39,19 +75,49 @@ class Node:
 
 
 class DummyNode(Node):
+
+    def __init__(self, value):
+        """ Dummy Node, don't retain a value provided
+
+        :param value: ignore
+        """
+        self.value = None
+
     def add_child(self, node_value):
+        """ Dummy Node, don't add child
+
+        :param node_value: ignore
+        :return: a dummy node
+        """
         return DummyNode(None)
 
     def is_leaf(self) -> bool:
+        """ Always return true, no children
+
+        :return: True
+        """
         return True
 
     def find(self, val):
+        """ Don't bother searching
+
+        :param val: search parameter
+        :return: None
+        """
         return None
 
     def get_children(self):
+        """ Return an empty list
+
+        :return: empty list
+        """
         return []
 
     def get_value(self):
+        """ Getter for  dummy value
+
+        :return: None
+        """
         return None
 
 
