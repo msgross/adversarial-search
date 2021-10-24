23f7cca7b1b3211b3031f7775af69b08d464ac78
mgross <mark@grossremarks.com>
2021-10-23 23:34:38 -0700

    fixing setup packages

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url='https://github.com/msgross/adversarial-searching',
     author='Mark Gross',
     author_email='mark@grossremarks.com',
-    packages=['algorithms, utils'],
+    packages=['algorithms', 'utils'],
     install_requires=[],
     version='0.1',
     license='MIT',
