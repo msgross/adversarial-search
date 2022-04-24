84b4a86f6b5c723fd7d29f065f75620ae0410ebd
Mark <mark@grossremarks.com>
2022-04-24 19:31:43 -0400

    Attempt to add in pydoc to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../algorithms'))
 
 
 # -- Project information -----------------------------------------------------
@@ -32,6 +32,8 @@ release = '1.0.0'
 # ones.
 extensions = [
     'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx_rtd_theme'
 ]
 
